3eabdc169390960e52a938bcf2ec0a7679db91b3
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-08-26 15:59:31 +0530

    Update Passing Pointers as Arguments.cpp

--- Passing Pointers as Arguments.cpp
+++ Passing Pointers as Arguments.cpp
@@ -1,1 +1,4 @@
+/* Pointer Scope#
+As we’ve learned in the Function section, function arguments are usually local in scope. Once we return from the function, all its variables are deleted forever.
+One solution was to pass external variables by reference using the & operator. This would alter the values of the variables outside the function scope.*/
 
