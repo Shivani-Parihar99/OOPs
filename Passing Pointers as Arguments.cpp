f8c343ba323d1196f06a9b065d032fc6fafd63b5
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-08-26 16:00:36 +0530

    Update Passing Pointers as Arguments.cpp

--- Passing Pointers as Arguments.cpp
+++ Passing Pointers as Arguments.cpp
@@ -1,4 +1,23 @@
 /* Pointer Scope#
 As we’ve learned in the Function section, function arguments are usually local in scope. Once we return from the function, all its variables are deleted forever.
 One solution was to pass external variables by reference using the & operator. This would alter the values of the variables outside the function scope.*/
+#include <iostream>
+using namespace std;
+
+void square(int &a){ // the function takes an integer and 
+  //replaces the value with its square
+  a = a * a;
+}
+
+int main() {
+  int a = 5;
+  cout << "The value of a before the function call: " << a << endl;
+  square(a);
+  cout << "The value of a after the function call: " << a << endl;
+}
+
+/* O/P
+The value of a before the function call: 5
+The value of a after the function call: 25 */
+
 
