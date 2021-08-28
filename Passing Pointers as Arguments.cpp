e48794551b6feed2b45e7c65930fe17b373b81b7
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-08-28 17:32:11 +0530

    Update Passing Pointers as Arguments.cpp

--- Passing Pointers as Arguments.cpp
+++ Passing Pointers as Arguments.cpp
@@ -21,3 +21,21 @@ The value of a before the function call: 5
 The value of a after the function call: 25 */
 
 
+#include <iostream>
+using namespace std;
+
+void square(int *a){ // the function takes a pointer and replaces the value with its square
+  if(a != NULL){
+    *a = (*a) * (*a);
+  }
+}
+
+int main() {
+  int *p = new int(5);
+  cout << "The value of p before the function call: " << *p << endl;
+  square(p);
+  cout << "The value of p after the function call: " << *p << endl;
+}
+/* O/P 
+
+
