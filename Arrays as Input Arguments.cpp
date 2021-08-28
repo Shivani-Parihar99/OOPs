262698e5d4bc3203606d9d4d36e66e183750462c
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-08-28 17:34:10 +0530

    Update Arrays as Input Arguments.cpp

--- Arrays as Input Arguments.cpp
+++ Arrays as Input Arguments.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+void doubleValues(int arr[], int size){
+// a function which doubles the values of all the elements in an array
+  for (int i = 0; i < size; i++){ 
+    arr[i] = arr[i] * 2;
+  }
+}
+
+int main() {
+  const int size = 10;
+  int arr[size];
+  
+  cout << "Original Values: ";
+  for (int i = 0; i < size; i++){
+    arr[i] = i;
+    cout << arr[i] << ", ";
+  }
+  cout << endl;
+  
+  doubleValues(arr, size);
+  
+  cout << "Doubled Values: ";
+  for (int i = 0; i < size; i++){
+    cout << arr[i] << ", "; // the original array has been changed
+  }
+}
+/* O/P 
+Original Values: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+Doubled Values: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, */
