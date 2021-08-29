dbd1255eea47e1da2253dc20a7b83fad4c27ee85
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-08-29 15:00:13 +0530

    Update Pointer Arithmetic.cpp

--- Pointer Arithmetic.cpp
+++ Pointer Arithmetic.cpp
@@ -1,1 +1,18 @@
+// Basic Addition and Subtraction
+#include <iostream>
+using namespace std;
 
+int main() {
+  int *p = new int(10);
+  cout << "p: " << p << endl;
+  cout << "value of p: " << *p << endl << endl;
+  
+  p = p + 1;
+  cout << "p: " << p << endl;
+  cout << "value of p: " << *p << endl << endl;
+}
+/* O/P p: 0x9e5c20
+value of p: 10
+
+p: 0x9e5c24
+value of p: 0 */
