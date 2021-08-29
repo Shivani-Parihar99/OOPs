2d9be8511710fc702b71514cb7a8d07e7a658b7a
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-08-29 15:02:08 +0530

    Update Pointer Arithmetic.cpp

--- Pointer Arithmetic.cpp
+++ Pointer Arithmetic.cpp
@@ -16,3 +16,20 @@ value of p: 10
 
 p: 0x9e5c24
 value of p: 0 */
+
+// Arrays and Pointer Arithmetic
+#include <iostream>
+using namespace std;
+
+int main() {
+  int *arr = new int[10];
+  
+  for(int i = 0; i < 10 ; i++){
+    arr[i] = i;
+  }
+  
+  cout << "arr[0]: " << *arr << endl;
+  cout << "arr[3]: " << *(arr + 3) << endl;
+}
+/* O/P arr[0]: 0
+arr[3]: 3 */
