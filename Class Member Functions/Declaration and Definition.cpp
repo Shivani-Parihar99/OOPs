4415c7fade7a39194b3f18700ed2939001891cef
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-09-22 12:37:54 +0530

    Update Declaration and Definition.cpp

--- Class Member Functions/Declaration and Definition.cpp
+++ Class Member Functions/Declaration and Definition.cpp
@@ -1,1 +1,14 @@
+class Rectangle {
+  int length;
+  int width;
 
+  public:
+  void setLength(int l){ // This function changes the value of length
+    length = l;
+  }
+  
+  int area(){
+    return length * width; // Only the values of the data members are
+                           // accessed and used to calculate the area
+  }
+};
