bc8643b4e35045def795e4e5d9bf22ad24054c83
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-09-22 12:40:09 +0530

    Update Get and Set Functions.cpp

--- Class Member Functions/Get and Set Functions.cpp
+++ Class Member Functions/Get and Set Functions.cpp
@@ -1,1 +1,35 @@
+class Rectangle {
+  int length;
+  int width;
 
+  public:
+  
+  // get and set for length
+  void setLength(int l);
+  int getLength();
+  
+  // get and set for width
+  void setWidth(int w);
+  int getwidth();
+  
+  int area();
+};
+
+
+void Rectangle::setLength(int l){ 
+  length = l;
+}
+int Rectangle::getLength(){ 
+  return length;
+}
+
+void Rectangle::setWidth(int w){ 
+  width = w;
+}
+int Rectangle::getWidth(){ 
+  return width;
+}
+
+int Rectangle::area(){
+  return length * width; 
+}
