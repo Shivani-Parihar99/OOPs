1756065bc9b8cb44ddd47a114672e4cdecb5dd72
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-09-22 12:39:00 +0530

    Update Declaration and Definition.cpp

--- Class Member Functions/Declaration and Definition.cpp
+++ Class Member Functions/Declaration and Definition.cpp
@@ -12,3 +12,27 @@ class Rectangle {
                            // accessed and used to calculate the area
   }
 };
+
+
+/* Scope Resolution Operator#
+The scope resolution operator (::) allows us to simply declare the member functions in the class and define them elsewhere in the code. */
+
+class Rectangle {
+  int length;
+  int width;
+
+  public:
+  
+  // We only write the declaration here
+  void setLength(int l);
+  int area();
+};
+
+// Somewhere else in the code
+void Rectangle::setLength(int l){ // Using the scope resolution operator
+  length = l;
+}
+
+int Rectangle::area(){
+  return length * width; 
+}
