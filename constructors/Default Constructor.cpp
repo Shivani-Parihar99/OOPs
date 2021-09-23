8b6800973f7f858e78512e742773a9d78af4d1ce
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-09-23 21:42:22 +0530

    Update Default Constructor.cpp

--- constructors/Default Constructor.cpp
+++ constructors/Default Constructor.cpp
@@ -1,1 +1,32 @@
 // Here, we have a Date class, with its default constructor, and we’ll create an object out of it in main():
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Date {
+  int day;
+  int month;
+  int year;
+
+  public:
+  // Default constructor
+  Date(){
+    // We must define the default values for day, month, and year
+    day = 0;
+    month = 0;
+    year = 0;
+  }
+
+  // A simple print function
+  void printDate(){ 
+    cout << "Date: " << day << "/" << month << "/" << year << endl;
+  }
+};
+
+int main(){
+  // Call the Date constructor to create its object;
+  
+  Date d; // Object created with default values!
+  d.printDate();
+}
+  
