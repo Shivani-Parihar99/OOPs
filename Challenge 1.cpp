f62c630230a67dcc98a41ddfeecf9bcbd930a769
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-10-16 23:08:30 +0530

    Create Challenge 1.cpp

---
+++ Challenge 1.cpp
@@ -0,0 +1,8 @@
+/* Problem Statement#
+Write a Rectangle class having two float type variables for length and height, a parametrized constructor and two member functions called perimeter and area which return perimeter and area of the rectangle respectively.
+
+Input#
+Length and width of the rectangle
+
+Output#
+Perimeter of rectangle and Area of rectangle */
