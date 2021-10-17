1c4dda4639b9f21c2410dd1f4937178b7c287db3
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-10-17 22:35:45 +0530

    Update Challenge 3.cpp

--- Challenge 3.cpp
+++ Challenge 3.cpp
@@ -1,1 +1,23 @@
 /* Implement a Calculator Class
+Write a C++ class called Calculator with
+
+private member variables:
+
+num1 and num2 (float type)
+And member functions:
+
+add(float n1, float n2), a function which returns the addition of two numbers
+
+Subtract(float n1, float n2), a function which returns the subtraction of n1 from n2
+
+multiply(float n1, float n2), a function which returns the multiplication of numbers
+
+divide(float n1, float n2), a function which returns the division of n2 by n1
+
+Define a default constructor which initializes both numbers by zeros
+
+Input#
+Pass floating point numbers in the member functions
+
+Output#
+Addition, Subtraction, division, and multiplication */
