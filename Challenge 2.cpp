c9be92945fae36835cdf8250c8c62e5f4be5c8f4
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-10-17 22:34:00 +0530

    Update Challenge 2.cpp

--- Challenge 2.cpp
+++ Challenge 2.cpp
@@ -1,1 +1,25 @@
 /* Calculate Student's Total Marks
+Write a C++ class called student with
+
+private member variables:
+
+name(string type)
+
+mark1 and mark2 (float type)
+
+And member functions:
+
+Get_Marks(int marknumber), a function which should return mark1 if marknumber equals 1 and mark2 otherwise.
+
+calc_total() function should take the two marks entered and return their sum.
+
+Also define two constructors:
+
+A default constructor that takes no parameters and initializes the values to zeros and null.
+A constructor that takes the three variables and sets them to given values.
+Input#
+Name of student, marks in first and second test
+
+Output#
+Sum of both marks */
+
