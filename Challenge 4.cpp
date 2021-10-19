eb29fbd9bffdeee302a73da9d9ad977462bd02d3
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-10-19 23:46:32 +0530

    Update Challenge 4.cpp

--- Challenge 4.cpp
+++ Challenge 4.cpp
@@ -20,4 +20,40 @@ float distance(int x1, int y1), a method which calculates the distance between t
 The distance between two points (x, y)(x,y) and (x1, y1)(x1,y1) can be calculated by the following formula:
 
 {distance} = sqrt{(x_1 - x)^2 + (y_1 - y)^2}*/
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+class Point {
+
+  // Private fields
+  private:
+  int x;
+  int y;
+
+  public:
+
+  // Default Constructor
+  Point() {
+    x = 0;
+    y = 0;
+  }
+
+  // Parameterized Constructor
+  Point(int x, int y) { 
+    this->x = x;
+    this->y = y;
+  }
+
+
+  double distance() {
+    double distance = sqrt(x*x + y*y);
+    return distance;
+  }
+
+  double distance(int x2, int y2) {
+    double distance = sqrt(((x2-x)*(x2-x))+((y2-y)*(y2-y)));
+    return distance;
+  }
+
+};
