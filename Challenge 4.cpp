6a636d84568ee20eac49f44585ba805cda529305
Shivani Parihar <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-10-19 23:46:13 +0530

    Update Challenge 4.cpp

--- Challenge 4.cpp
+++ Challenge 4.cpp
@@ -1,1 +1,23 @@
 /* Calculate Distance Between Points
+You have to implement a class called Point that represents a specific point in the x-y plane. It should contain the following:
+
+fields:
+
+x( integer type)
+
+y( integer type)
+
+methods:
+
+default constructor that initializes the point at (0, 0)(0,0)
+
+parameterized constructor that takes input x and y and initializes the point to the respective coordinates.
+
+float distance(), a method which calculates the distance of the point (represented by the object) from the origin, i.e. (0, 0)(0,0)
+
+float distance(int x1, int y1), a method which calculates the distance between the point represented by the class object and (x1, y1)(x1,y1)
+
+The distance between two points (x, y)(x,y) and (x1, y1)(x1,y1) can be calculated by the following formula:
+
+{distance} = sqrt{(x_1 - x)^2 + (y_1 - y)^2}*/
+
